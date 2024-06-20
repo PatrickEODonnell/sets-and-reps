835289cc7f917a16b1dcf648842b2e84b16b1ad2
Patrick ODonnell <pej.odonnell@gmail.com>
2024-06-19 20:22:13 -0400

    Fix logic for updating minRemaining, secRemaining and secPerSet

--- src/libs/siteParams.js
+++ src/libs/siteParams.js
@@ -5,11 +5,12 @@ export const useSetParamsStore = defineStore('setParams',() => {
     let sets = ref(4)
     let set = ref(1)
     let minPerSet = ref(3)
-    let secPerSet = ref(60)
+    let secPerSet = ref(180)
     let setType = ref("Standard")
     let minRemaining = ref(3)
     let secRemaining = ref(0)
     let remainingSets = ref(4)
+    let timerIsRunning = ref(false)
 
     // Getters
     const getMinPerSet = computed(() => minPerSet.value)
@@ -22,6 +23,7 @@ export const useSetParamsStore = defineStore('setParams',() => {
 
     function updateMinPerSet(min){
         minPerSet.value = min
+        secPerSet.value = min * 60
         minRemaining.value = ~~((min * 60)/60)
         secRemaining.value = (min * 60) % 60
     }
@@ -32,15 +34,21 @@ export const useSetParamsStore = defineStore('setParams',() => {
         setType.value = setType
     }
     function startStandardTimer(){
+        timerIsRunning.value = true
         const x = setInterval(() => {
             secPerSet.value -= 1
+            minRemaining.value = ~~(secPerSet.value/60)
+            secRemaining.value = secPerSet.value % 60
             console.log("secPerSet",secPerSet.value)
             console.log("remainingSets",remainingSets.value)
+            console.log("minRemaining", minRemaining.value)
+            console.log("secRemaining", secRemaining.value)
             if (secPerSet.value <= 0){
                 remainingSets.value -= 1
                 secPerSet.value = minPerSet.value * 60
                 if (remainingSets.value <= 0) {
                     secPerSet.value = 0
+                    timerIsRunning.value = false
                     clearInterval(x);
                 }
                 set.value += set.value < sets.value ? 1 : 0
