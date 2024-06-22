18c3c9d10a1b6bb46b773f9e7d5c3a712c733c04
Patrick ODonnell <pej.odonnell@gmail.com>
2024-06-22 19:59:13 -0400

    PWA Setup

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,39 @@ export default defineConfig({
   plugins: [
     vue(),
     VueDevTools(),
-    VitePWA({ registerType: 'autoUpdate' }),
+    VitePWA({ registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png'],
+      manifest: {
+        name: 'Sets and Reps',
+        short_name: 'SnR',
+        description: 'Workout timer with Standard, Superset, EMOM, and Tabata timing parameters plus ability to add and save resistance exercises and workouts',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any'
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'maskable'
+          }
+        ]
+      }      
+     }),
   ],
   resolve: {
     alias: {
