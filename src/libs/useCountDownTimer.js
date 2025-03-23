cda5d2be6ff65ec36b2c67ba6692ebf093e8d787
Patrick O'Donnell <pej.odonnell@gmail.com>
2025-03-23 16:34:50 -0400

    Enable sound on Countdown component

--- src/libs/useCountDownTimer.js
+++ src/libs/useCountDownTimer.js
@@ -1,11 +1,24 @@
 import { ref } from "vue"
+import { useSound } from "@vueuse/sound";
+import countdownsound from '../assets/short-beep-countdown.wav';
+import { useSetParamsStore } from "@/libs/siteParams";
+
 export function useCountDownTimer() {
+    const store = useSetParamsStore();
     const countDownSeconds = 10;
     const currentSecond = ref(10);
+    const {play} = useSound(countdownsound);
+    let playingSound = false;
+
     function runCountdownTimer(){
         const x = setInterval(() => {
             currentSecond.value -= 1
+            if (currentSecond.value < 4 && store.soundEnabled && !playingSound){
+                playingSound = true;
+                play();
+            }
             if (currentSecond.value <= 0 ){
+                playingSound = false;
                 clearInterval(x);
             }
         }, 1000);
